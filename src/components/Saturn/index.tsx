b83e2cd0b19a4966bbcb9fd472ca808932cd66a7
jessieleigh30 <jessie.leigh30@gmail.com>
2021-05-24 12:11:24 -0600

    saturn has it's texture

--- src/components/Saturn/index.tsx
+++ src/components/Saturn/index.tsx
@@ -5,15 +5,12 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Saturn = () => {
   const group = useRef();
-  const { nodes } = useLoader(GLTFLoader, 'models/saturn/scene.gltf');
+  const { nodes } = useLoader(GLTFLoader, 'models/saturn/Saturn.gltf');
   const texture = useLoader(
     THREE.TextureLoader,
-    '/models/crane/textures/PapelOrigami_baseColor.jpeg'
+    '/models/saturn/textures/SaturnSurface_Color.png'
   );
-  const texture2 = useLoader(
-    THREE.TextureLoader,
-    '/models/crane/textures/Sombra_baseColor.png'
-  );
+
   useFrame(() => {
     // group.current.rotation.z += Math.PI / 2.75;
     // group.current.rotation.x = Math.PI / -1.5;
@@ -30,46 +27,22 @@ const Saturn = () => {
         rotation={[0, 0, 0.5]}
       >
         <mesh visible geometry={nodes.Sphere.geometry}>
-          <meshStandardMaterial
-            attach="material"
-            color="#021945"
-            map={texture}
-          />
+          <meshStandardMaterial attach="material" map={texture} />
         </mesh>
         <mesh visible geometry={nodes.Sphere1.geometry}>
-          <meshStandardMaterial
-            attach="material"
-            color="#021945"
-            map={texture}
-          />
+          <meshStandardMaterial attach="material" map={texture} />
         </mesh>
         <mesh visible geometry={nodes.Sphere2.geometry}>
-          <meshStandardMaterial
-            attach="material"
-            color="#021945"
-            map={texture}
-          />
+          <meshStandardMaterial attach="material" map={texture} />
         </mesh>
         <mesh visible geometry={nodes.Sphere3.geometry}>
-          <meshStandardMaterial
-            attach="material"
-            color="#021945"
-            map={texture}
-          />
+          <meshStandardMaterial attach="material" map={texture} />
         </mesh>
         <mesh visible geometry={nodes.Tube.geometry}>
-          <meshStandardMaterial
-            attach="material"
-            color="#021945"
-            map={texture}
-          />
+          <meshStandardMaterial attach="material" map={texture} />
         </mesh>
         <mesh visible geometry={nodes.Tube1.geometry}>
-          <meshStandardMaterial
-            attach="material"
-            color="#021945"
-            map={texture}
-          />
+          <meshStandardMaterial attach="material" map={texture} />
         </mesh>
       </group>
     )
