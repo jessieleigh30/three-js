eb0ca9695a5c442173b4742954d1e231967768b0
jessieleigh30 <jessie.leigh30@gmail.com>
2021-05-28 16:32:30 -0600

    tiny canvas problem

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,83 @@
-import React from 'react';
+import React, { Suspense, useRef, useEffect, useMemo } from 'react';
+import * as THREE from 'three/src/Three';
+import Saturn from '@components/Saturn';
+import state from '@components/State';
+import { Block } from '@components/Block';
+import { Canvas, useFrame } from '@react-three/fiber';
 
-import SaturnScene from '@components/SaturnScene';
+function Loading() {
+  return (
+    <mesh rotation={[0, 0, 0]}>
+      <boxGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
+}
 
-const ExampleView = () => {
+const Stars = () => {
+  const group = useRef();
+  const mesh = useRef();
+  const [geo, mat, vertices, coords] = useMemo(() => {
+    const geo = new THREE.OctahedronGeometry();
+    const mat = new THREE.MeshBasicMaterial({
+      color: new THREE.Color('lightblue'),
+    });
+    const coords = new Array(2000)
+      .fill()
+      .map((i) => [
+        Math.random() * 800 - 400,
+        Math.random() * 800 - 400,
+        Math.random() * 800 - 400,
+      ]);
+    return [geo, mat, vertices, coords];
+  }, []);
+  useFrame(() => {
+    //change to mesh to try and get the individual stars to move
+    group.current.rotation.y += 0.001;
+  });
+  return (
+    <group ref={group}>
+      {coords.map(([p1, p2, p3], i) => (
+        <mesh
+          key={i}
+          geometry={geo}
+          material={mat}
+          position={[p1, p2, p3]}
+          ref={mesh}
+        />
+      ))}
+    </group>
+  );
+};
+
+const HomeView = () => {
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
   return (
     <>
-      <div>
-        <SaturnScene />
+      <Canvas camera={{ fov: 75, position: [0, 0, 0] }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[0, 0, 0]} color="white" />
+        <Suspense fallback={<Loading />}>
+          <Stars />
+          <Block offset={2} factor={1.5}>
+            <Saturn />
+          </Block>
+        </Suspense>
+      </Canvas>
+      <div ref={scrollArea} onScroll={onScroll}>
+        <div style={{ height: `${state.pages * 100}vh` }} />
       </div>
     </>
   );
 };
 
-export default ExampleView;
+export default HomeView;
